8eeba977fd6a7ba1e8db3d6d70867660e41160e7
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-26 12:10:32 +0000

    Add OpenAPI documentation to Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Expense Tracker API",
+     *      description="Laravel API for Expense Tracker",
+     *      @OA\Contact(
+     *          email="acharouaoumohamed @gmail.com"
+     *     ),   
+     * )
+     */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
